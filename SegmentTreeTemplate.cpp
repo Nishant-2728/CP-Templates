976086c079a838061660fcdbbfabad1406aa2f58
Nishant Kumar Biswal <biswalnishant2003@gmail.com>
2026-05-31 00:56:18 +0530

    Refactor SegmentTree to use long long for indices

--- SegmentTreeTemplate.cpp
+++ SegmentTreeTemplate.cpp
@@ -1,35 +1,35 @@
 class SegmentTree{
 public:
-    vector<int>seg;
-    SegmentTree(int n){
+    vector<ll>seg;
+    SegmentTree(ll n){
         seg.resize(4*n+1);
     }
-    void build(int ind,int low,int high,int arr[]){
-    if(low==high){
-        seg[ind]=arr[low];
-        return;
+    void build(ll ind,ll low,ll high,vector<ll>&arr){
+        if(low==high){
+            seg[ind]=arr[low];
+            return;
+        }
+        ll mid=(low+high)/2;
+        build(2*ind+1,low,mid,arr);
+        build(2*ind+2,mid+1,high,arr);
+        seg[ind]=min(seg[2*ind+1],seg[2*ind+2]);
     }
-    int mid=(low+high)/2;
-    build(2*ind+1,low,mid,arr);
-    build(2*ind+2,mid+1,high,arr);
-    seg[ind]=min(seg[2*ind+1],seg[2*ind+2]);
-    }
-    int query(int ind,int low,int high,int l,int r){
+    ll query(ll ind,ll low,ll high,ll l,ll r){
         if(high<l || r<low)
             return 1e9;
         if(low>=l && high<=r)
             return seg[ind];
-        int mid=(low+high)/2;
-        int left=query(2*ind+1,low,mid,l,r);
-        int right=query(2*ind+2,mid+1,high,l,r);
-        return min(left,right);
+        ll mid=(low+high)/2;
+        ll left=query(2*ind+1,low,mid,l,r);
+        ll right=query(2*ind+2,mid+1,high,l,r);
+        ll min(left,right);
     }
-    void update(int ind,int low,int high,int index,int val){
+    void update(ll ind,ll low,ll high,ll index,ll val){
         if(low==high){
             seg[ind]=val;
             return;
         }
-        int mid=(low+high)/2;
+        ll mid=(low+high)/2;
         if(index<=mid)
             update(2*ind+1,low,mid,index,val);
         else
